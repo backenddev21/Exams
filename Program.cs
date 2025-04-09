716eed0e13ba5a0d3dc12ab17fda70d119e48e57
backenddev21 <jnavarro.ph@gmail.com>
2025-04-09 11:07:56 +0800

    clean program remove commented lines

--- Program.cs
+++ Program.cs
@@ -15,15 +15,12 @@ namespace elevator
                 new(3),
                 new(4)
             };
+            
             var controller = new ElevatorController(elevators);
-            //  controller.RequestElevator(3, 1, Direction.Down); 
-            //  controller.RequestElevator(1, 3, Direction.Up);
-            //  controller.RequestElevator(5, 1, Direction.Down);
-            //  controller.RequestElevator(2, 7, Direction.Down);
-
-            controller.RequestElevator(3, 1, Direction.Down);
-            controller.RequestElevator(4, 6, Direction.Up);
-            controller.RequestElevator(2, 10, Direction.Up);
+             controller.RequestElevator(3, 1, Direction.Down); 
+             controller.RequestElevator(1, 3, Direction.Up);
+             controller.RequestElevator(5, 1, Direction.Down);
+             controller.RequestElevator(2, 7, Direction.Down);
              
              await controller.StartElevators();
         }
